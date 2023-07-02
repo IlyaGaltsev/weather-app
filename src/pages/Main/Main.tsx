6fd1f39844c0efab195935bc52da57303d11b6c7
Ilya Galtsev <ilyandreev771@gmail.com>
2023-07-02 20:11:56 +0300

    Проверка

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -49,13 +49,39 @@ const Main = () => {
     fetchWeatherData()
     getCurrentWeather()
   }, [])
+
+  const getDaData = () => {
+    var url = 'https://cleaner.dadata.ru/api/v1/clean/address'
+    var token = '786f86c8fa7e5b49d9a00e9188dcdb352a15c739'
+    var secret = '1c7e49d03ceb23552d5f45af92de0be9b642756f'
+    var query = 'мск сухонска 11/-89'
+
+    var options = {
+      method: 'POST',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Token ' + token,
+        'X-Secret': secret
+      },
+      body: JSON.stringify([query])
+    }
+
+    axios
+      .get(url, options)
+      // .then(response => response.())
+      .then(result => console.log(result))
+      .catch(error => console.log('error', error))
+  }
   return (
-    <div>
+    <div className="main__wrapper">
+      <input />
       <CurrentWeather
         current={currentWeather?.current}
         location={currentWeather?.location}
       />
 
+      <button onClick={getDaData}>getDadata</button>
       <button onClick={getCurrentWeather}>getWeather</button>
     </div>
   )
